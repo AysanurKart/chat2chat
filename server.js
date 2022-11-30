f8a7e420377e6f253e4e558ef387999ec08c6332
AysanurKart <ayka21ab@student.cbs.dk>
2022-12-01 00:58:26 +0100

    database færdig

--- server.js
+++ server.js
@@ -1,11 +1,25 @@
+const express = require("express");
+const session = require("express-session");
+const bodyParser = require("body-parser");
+const path = require("path");
+const app = express();
+const port = 4000;
 const sqlite3 = require("sqlite3").verbose();
+var crypto = require("crypto");
+const http = require("http");
+const socketio = require("socket.io");
 
-const db = new sqlite3.Database('./db.sqlite',(err) => {
-    if (err) {
-        return console.error(err.message);
+const server = http.createServer(app);
+
+const io = require("socket.io")(server, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"]
     }
-    console.log('Connected to the in-memory sqlite database')
 });
+
+//sqlite ting
+const db = new sqlite3.Database('./db.sqlite');
 
 db.serialize(function() {
     console.log('creating database if they don/t exist');
@@ -13,7 +27,9 @@ db.serialize(function() {
 }); 
 
 //Tilføjer bruger til Database 
-const addUsertoDatabase = (username, password) => {
+
+
+const addUserToDatabase = (username, password) => {
     db.run(
         'insert into users(username, password) values (?,?)',
         [username, password], 
@@ -33,7 +49,7 @@ const getUserByUsername = (userName) => {
             [userName],
             (err, rows) => {
                 if(err) {
-                    console.log(err);
+                    console.error(err);
                     return reject (err);
                 }
                 return resolve(rows);
@@ -41,3 +57,98 @@ const getUserByUsername = (userName) => {
         );
     })
 }
+
+const hashPassword = (password) => {
+    const md5sum = crypto.createHash('md5');
+    const salt = 'Some salt for the hash';
+    return md5sum.update(password + salt).digest('hex');
+}
+
+app.use(express.static(__dirname + '/public'))
+
+app.use(
+    session({
+        secret: "Keep it secret", 
+        name: "uniqueSessionID",
+        saveUninitialized: false,
+        resave: true,
+    })
+);
+
+app.get('/', (req, res) => {
+    if (req.session.loggedIn) {
+        return res.redirect('/chat.html');
+    } else {
+        return res.sendFile("login.html", {root: path.join(__dirname, "public")});
+    }
+});
+
+// Et dashboard som kun brugere med 'loggedIn' = true i session kan se
+app.get("/chat.html", (req, res) => {
+    if(req.session.loggedIn) {
+        res.setHeader("Content-Type", "text/html");
+        res.write("Welcome" + req.session.username + "to your dashboard");
+        res.write('<a href=/"logout">Logout</a>')
+        return res.end();
+    } else {
+        return res.redirect("/");
+    }
+});
+
+app.post("/authenticate", bodyParser.urlencoded({extended:true}), async(req, res) => {
+
+    const user = await getUserByUsername(req.body.username);
+
+    if(user.length === 0) {
+        console.log('no user found');
+        return res.redirect("/");
+      }
+
+    if (user[0].password === hashPassword(req.body.password)) {
+        req.session.loggedIn = true;
+        req.session.username = req.body.username;
+        console.log(req.session);
+        res.redirect("/chat.html");
+    } else {
+        // Sender en error 401 (unauthorized) til klienten
+        return  res.sendStatus(401);
+    }
+  });
+
+  app.get("/logout", (req, res) => {
+    req.session.destroy((err) => {});
+    return res.send("Thank you! Visit again");
+  });
+
+  app.get("/signup", (req, res) => {
+    if (req.session.loggedIn) {
+        return res.redirect("/chat.html");
+    } else {
+        return res.sendFile("signup.html", { root: path.join(__dirname, "public") });
+    }
+  });
+
+  app.post("/signup", bodyParser.urlencoded({extended: true}), async (req, res) => {
+    const user = await getUserByUsername(req.body.username)
+    if (user.length > 0) {
+      return res.send('Username already exists');
+    }
+    
+addUserToDatabase(req.body.username, hashPassword(req.body.password));
+    res.redirect('/');
+
+  }) 
+
+
+
+
+
+
+//HTTP ting 
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + "/chat.html");
+})
+
+server.listen(4000, function() {
+    console.log('Server runs on PORT 4000')
+})
