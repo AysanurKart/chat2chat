a9d989e11b4647b93d981734fc3ed3fa097cfc9c
AysanurKart <ayka21ab@student.cbs.dk>
2022-12-21 09:09:42 +0100

    finished most changes

--- server.js
+++ server.js
@@ -1,217 +1,249 @@
-const express = require("express");
-const session = require("express-session");
-const bodyParser = require("body-parser");
-const path = require("path");
+const express = require('express');
+const session = require('express-session');
+const bodyParser = require('body-parser');
+const path = require('path');
 const app = express();
 const port = 4000;
-const sqlite3 = require("sqlite3").verbose();
-var crypto = require("crypto");
-const http = require("http");
-const socketio = require("socket.io");
-const sqlite = require("sqlite3");
-const formatMessage = require('./utility/beskeder');
-const server = http.createServer(app);
+const sqlite3 = require('sqlite3').verbose();
+var crypto = require('crypto');
+const http = require('http');
+const socketio = require('socket.io');
 const cookieParser = require('cookie-parser');
 
-app.use(cookieParser());
+const server = http.createServer(app);
 
-const io = require("socket.io")(server, {
-    cors: {
-        origin: "*",
-        methods: ["GET", "POST"]
-    }
+const io = require('socket.io')(server, {
+   cors: {
+      origin: '*',
+      methods: ['GET', 'POST'],
+   },
 });
 
 const {
-    userJoin, 
-    getCurrentUser, 
-    userLeave, 
-    getRoomUsers,
-} = require('./utility/users');
+   userJoin,
+   getCurrentUser,
+   userLeave,
+   getRoomUsers,
+} = require('./utility/user');
+
+const formatMessage = require('./utility/messages');
+
+app.use(cookieParser());
+
+//set static folder
+app.use(express.static(path.join(__dirname, 'public')));
 
 const botName = 'Admin';
 
-// run when client connects
+//run when client connects
 io.on('connection', (socket) => {
-    socket.on('joinRoom', ({username, room}) => {
-        const user = userJoin(socket.id, username, room);
+   socket.on('joinRoom', ({username, room }) => {
+      const user = userJoin(socket.id, username, room);
 
-        socket.join(user.room);
+      socket.join(user.room);
 
-        //welcome current user
-        socket.emit('message', formatMessage(botName, 'Velkommen til Chat2chat'));
+      //Welcome current user
+      socket.emit('message', formatMessage(botName, 'Welcome to LetzChat!'));
 
       //Broadcast when a user connects. Viser til alle undtagen selve useren som forbinder
-
       socket.broadcast
-      .to(user.room)
-      .emit(
-        'message',
-        formatMessage(botName, `${user.username} har til sluttet sig chatten`)
-      );
+         .to(user.room)
+         .emit(
+            'message',
+            formatMessage(botName, `${user.username} has joined the chat`)
+         );
 
       //send users and room info
       io.to(user.room).emit('roomUsers', {
-        users: getRoomUsers(user.room),
+         users: getRoomUsers(user.room),
       });
-    });
+   });
 
-    //listen for chat message
-    socket.on('chatMessage', (msg) => {
-        const user = getCurrentUser(socket.id);
+   //Listen for chat message
+   socket.on('chatMessage', (msg) => {
+      const user = getCurrentUser(socket.id);
 
-        io.to(user.room).emit('message', formatMessage(user.username, msg));
-    });
+      io.to(user.room).emit('message', formatMessage(user.username, msg));
+   });
 
-    //runs when client disconnects
-    socket.on('disconnect', () => {
-        const user = userLeave(socket.id);
+   //runs when client disconnects
+   socket.on('disconnect', () => {
+      const user = userLeave(socket.id);
 
-        if(user){
-            io.to(user.room).emit(
-                'message',
-                formatMessage(botName, `${user.username} har forladt chatten`)
-            );
+      if (user) {
+         io.to(user.room).emit(
+            'message',
+            formatMessage(botName, `${user.username} left the chat`)
+         );
 
-            //send users and room info
-            io.to(user.room).emit('roomUsers', {
-                users: getRoomUsers(user.room),
-            });
-        }
-    });
+         //send users and room info
+         io.to(user.room).emit('roomUsers', {
+            users: getRoomUsers(user.room),
+         });
+      }
+   });
 });
 
 //sqlite ting
 const db = new sqlite3.Database('./db.sqlite');
 
-db.serialize(function() {
-    console.log('creating database if they don/t exist');
-    db.run('Create table if not exists users(userID interger primary key, username text not null, password text not null)');
-}); 
+db.serialize(function () {
+   console.log('creating database if they don/t exist');
+   db.run(
+      'Create table if not exists users(userID interger primary key, username text not null, password text not null)'
+   );
+});
 
+//Tilføjer bruger til Database
 
-//Tilføjer bruger til Database 
 const addUserToDatabase = (username, password) => {
-    db.run(
-        'insert into users(username, password) values (?,?)',
-        [username, password], 
-        function(err) {
-            if (err) {
-                console.log(err);
-            }
-        }
-    );
+   db.run(
+      'insert into users(username, password) values (?,?)',
+      [username, password],
+      function (err) {
+         if (err) {
+            console.log(err);
+         }
+      }
+   );
+};
 
-}
+const deleteUserfromDatabase = (username, password) => {
+   db.run(
+      'select * from users where userName=(?)',
+      [username, password],
+      function(err) {
+      if(err){
+          winston.error(err);
+      }
+      else{
+          console.log("Successful");
+      }
+
+  });
+};
 
 const getUserByUsername = (userName) => {
-    //smart måde at konvertere fra callback til promise: 
-    return new Promise((resolve, reject) => {
-        db.all(
-            'select * from users where userName=(?)', 
-            [userName],
-            (err, rows) => {
-                if(err) {
-                    console.error(err);
-                    return reject (err);
-                }
-                return resolve(rows);
+   //smart måde at konvertere fra callback til promise:
+   return new Promise((resolve, reject) => {
+      db.all(
+         'select * from users where userName=(?)',
+         [userName],
+         (err, rows) => {
+            if (err) {
+               console.error(err);
+               return reject(err);
             }
-        );
-    })
-}
+            return resolve(rows);
+         }
+      );
+   });
+};
 
 const hashPassword = (password) => {
-    const md5sum = crypto.createHash('md5');
-    const salt = 'Some salt for the hash';
-    return md5sum.update(password + salt).digest('hex');
-}
+   const md5sum = crypto.createHash('md5');
+   const salt = 'Some salt for the hash';
+   return md5sum.update(password + salt).digest('hex');
+};
 
-app.use(express.static(__dirname + '/public'))
+app.use(express.static(__dirname + '/public'));
 
 app.use(
-    session({
-        secret: "Keep it secret", 
-        name: "uniqueSessionID",
-        saveUninitialized: false,
-        resave: true,
-    })
+   session({
+      secret: 'Keep it secret',
+      name: 'uniqueSessionID',
+      saveUninitialized: false,
+      resave: true,
+   })
 );
 
 app.get('/', (req, res) => {
-    if (req.session.loggedIn) {
-        return res.redirect('/chat.html');
-    } else {
-        return res.sendFile("login.html", {root: path.join(__dirname, "public")});
-    }
+   if (req.session.loggedIn) {
+      return res.redirect('/chat.html');
+   } else {
+      return res.sendFile('login.html', {
+         root: path.join(__dirname, 'public'),
+      });
+   }
 });
 
-// Et dashboard som kun brugere med 'loggedIn' = true i session kan se
-app.get("/chat.html", (req, res) => {
-    if(req.session.loggedIn) {
-        res.setHeader("Content-Type", "text/html");
-        res.write("Welcome" + req.session.username + "to your dashboard");
-        res.write('<a href=/"logout">Logout</a>')
-        return res.end();
-    } else {
-        return res.redirect("/");
-    }
-});
-
-app.post("/authenticate", bodyParser.urlencoded({extended:true}), async(req, res) => {
-
-    const user = await getUserByUsername(req.body.username);
-
-    if(user.length === 0) {
-        console.log('no user found');
-        return res.redirect("/");
+app.post(
+   '/authenticate',
+   bodyParser.urlencoded({ extended: true }),
+   async (req, res) => {
+      const user = await getUserByUsername(req.body.username);
+      if (user.length === 0) {
+         console.log('no user found');
+         return res.redirect('/');
       }
 
-    if (user[0].password === hashPassword(req.body.password)) {
-        req.session.loggedIn = true;
-        req.session.username = req.body.username;
-        console.log(req.session);
-        res.redirect("/chat.html");
-    } else {
-        // Sender en error 401 (unauthorized) til klienten
-        return  res.sendStatus(401);
-    }
-  });
+      if (user[0].password === hashPassword(req.body.password)) {
+         req.session.loggedIn = true;
+         req.session.username = req.body.username;
+         res.cookie('room', req.body.room, { maxAge: 99999999 });
+         res.cookie('name', req.body.username, { maxAge: 99999999 });
+         res.redirect('/chat.html');
+      } else {
+         // Sender en error 401 (unauthorized) til klienten
+         return res.sendStatus(401);
+      }
+   }
+);
 
-  app.get("/logout", (req, res) => {
-    req.session.destroy((err) => {});
-    return res.send("Thank you! Visit again");
-  });
+app.get('/logout', function(req, res) {
+   req.session.destroy(function(err){
+      if(err){
+         console.log(err);
+      }else{
+          res.redirect('/login.html');
+      }
+   });
+});
 
-  app.get("/signup", (req, res) => {
-    if (req.session.loggedIn) {
-        return res.redirect("/chat.html");
-    } else {
-        return res.sendFile("signup.html", { root: path.join(__dirname, "public") });
-    }
-  });
+app.get('/signup', (req, res) => {
+   if (req.session.loggedIn) {
+      return res.redirect('/chat.html');
+   } else {
+      return res.sendFile('signup.html', {
+         root: path.join(__dirname, 'public'),
+      });
+   }
+});
 
-  app.post("/signup", bodyParser.urlencoded({extended: true}), async (req, res) => {
-    const user = await getUserByUsername(req.body.username)
-    if (user.length > 0) {
-      return res.send('Username already exists');
-    }
+app.post(
+   '/signup',
+   bodyParser.urlencoded({ extended: true }),
+   async (req, res) => {
+      const user = await getUserByUsername(req.body.username);
+      if (user.length > 0) {
+         return res.send('Username already exists');
+      }
+      addUserToDatabase(req.body.username, hashPassword(req.body.password));
+      res.redirect('/');    
+   } 
+);
+
+app.delete('/delete', async (req, res) => {
     
-addUserToDatabase(req.body.username, hashPassword(req.body.password));
-    res.redirect('/');
+   const deleteUser = await deleteUserfromDatabase(req.params.username); 
+ 
+   Product.deleteOne({
+     _id: productId
+   }, (error, result) => {
+     if(error) {
+       res.json({error: 'Unable to delete product'})
+     } else {
+       res.json({success: true, message: 'Product deleted successfully!'})
+     }
+   })
+ 
+ })
 
-  }) 
+//HTTP ting
+app.get('/', function (req, res) {
+   res.sendFile(__dirname + '/chat.html');
+});
 
-
-
-
-
-
-//HTTP ting 
-app.get('/', function(req, res) {
-    res.sendFile(__dirname + "/chat.html");
-})
-
-server.listen(4000, function() {
-    console.log('Server runs on PORT 4000')
-})
+server.listen(4000, function () {
+   console.log('Server runs on PORT 4000');
+});
--- utility/user.js
+++ utility/user.js
@@ -1,35 +1,35 @@
 const users = [];
 
-//Tilføjer brugere til chatten 
-function userJoin(id, username, room){
-    const user = {id, username, room};
-
+//join user to chat 
+function userJoin(id, username, room) {
+    const user= {id, username, room};
+    
     users.push(user);
 
     return user;
 }
 
-//få fat i nuværende bruger
+//get current user
 function getCurrentUser(id) {
     return users.find(user => user.id === id);
 }
 
-//bruger forlader chatten 
+//User leaves chat 
 function userLeave(id){
     const index = users.findIndex(user => user.id === id);
 
-    if(index!==-1){
+    if(index !== -1) {
         return users.splice(index,1)[0];
     }
 }
 
-//få fat room users
-function getRoomUsers(room){
-    return users.filter(user => user.room === room);
+//get room users 
+function getRoomUsers(room) {
+    return users.filter(user => user.room === room); 
 }
 
 module.exports = {
-    userJoin, 
+    userJoin,
     getCurrentUser,
     userLeave,
     getRoomUsers
